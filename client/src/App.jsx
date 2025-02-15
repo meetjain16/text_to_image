b313dc2bd62a6b7a27653bcdc41547158a22eff3
meetjain <jainmeet162003@gmail.com>
2025-02-15 20:08:26 +0530

    desc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-100 to-orange-100'>
 <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
---
+++ client/src/components/Description.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Description = () => {
+    return (
+
+        <div>
+            <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
+
+                <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI images </h1>
+                <p className=' text-gray-600 mb-8'> Transform words into stunning images</p>
+            </div>
+            <div className='flex flex-col gap-5 md:gap-14 md:flex-row items-center'>
+                <img src={assets.menu_icon} className='w-80 xl:w-96 rounded-lg' alt="" />
+                <div>
+                    <h2 className='text-3xl font-medium max-w-lg mb-4'>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, nam.
+                    </h2 >
+                    <p className='text-gray-600 mb-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et amet cumque placeat voluptates corrupti distinctio repudiandae magni dolorem. Iste unde fuga laboriosam aspernatur sunt magnam facere possimus, perferendis minima explicabo.</p>
+                    <p className='text-gray-600 '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam nihil voluptatum dolores cum distinctio magnam quo itaque eveniet deserunt fuga corrupti, ipsam doloribus praesentium voluptatibus! Corrupti, quibusdam! Sunt, veritatis nesciunt.</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Description
