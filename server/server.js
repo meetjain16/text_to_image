74cb3818bf95e96e9b09f5d198270d1e152420a2
meetjain <jainmeet162003@gmail.com>
2025-02-17 22:03:40 +0530

    server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 
 import connectDB from './config/mongodb.js'
 import userRouter from './routrs/userRoutes.js'
+import imageRouter from './routrs/imageRoutes.js'
 
 const PORT=process.env.PORT ||4000
 const app =express()
@@ -15,6 +16,7 @@ app.use(cors())
 await connectDB()
 
 app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 
 app.get('/',(req,res)=>res.send("API Workinggggg"))
 
