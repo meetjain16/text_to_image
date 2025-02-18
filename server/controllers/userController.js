af1da367595fddec95edb162d18b2521559f6f8d
meetjain <jainmeet162003@gmail.com>
2025-02-18 20:18:08 +0530

    razorpay

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,6 +4,9 @@ import bcrypt from 'bcrypt'
 
 import jwt from 'jsonwebtoken'
 
+import razorpay from 'razorpay'
+import transactionModel from "../models/transactionModel.js";
+import { option } from "framer-motion/client";
 
 const registerUser = async (req, res) => {
     try {
@@ -65,14 +68,14 @@ const loginUser = async (req, res) => {
 }
 
 
-const userCredits =async(req,res)=>{
+const userCredits = async (req, res) => {
     try {
-        const {userId}=req.body
+        const { userId } = req.body
 
 
-        const user=await userModel.findById(userId)
+        const user = await userModel.findById(userId)
 
-        res.json({success:true, credits:user.creditBalance,user: {name:user.name} })
+        res.json({ success: true, credits: user.creditBalance, user: { name: user.name } })
     } catch (error) {
         console.log(error)
         res.json({ success: false, message: error.message })
@@ -80,4 +83,111 @@ const userCredits =async(req,res)=>{
     }
 
 }
-export {registerUser, loginUser,userCredits}
+
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+
+
+});
+
+const paymentRazorpay = async (req, res) => {
+    try {
+        const { userId, planId } = req.body
+        const userData = await userModel.findById(userId)
+
+        if (!userId || !planId) {
+            return res.json({
+                success: false, message: "missing details"
+            })
+
+        }
+
+        let credits, plan, amount, date
+
+        switch (planId) {
+            case 'basic':
+                plan = 'basic'
+                credits = 10
+                amount = 10
+                break;
+            case 'Advanced':
+                plan = 'Advanced'
+                credits = 100
+                amount = 100
+                break;
+            case 'Pro':
+                plan = 'Pro'
+                credits = 1000
+                amount = 1000
+                break;
+
+
+
+            default:
+                return res.json({success:false,message:'plan not found'});
+        }
+
+        date=Date.now();
+
+        const transactionData={
+            userId,plan,amount,credits ,date
+
+        }
+
+        const newTransaction=await transactionModel.create(transactionData)
+
+
+        const options={
+            amount:amount *100,
+            currency:process.env.CURRENCY,
+            receipt:newTransaction._id
+        }
+        await razorpayInstance.orders.create(options,(error,order)=>{
+
+if (error) {
+    console.log(error);
+    return res.json({success:false,message:error})
+}
+res.json({success:true,order})
+
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+const verifyRazorpay=async(req,res)=>{
+    try {
+        const {razorpay_order_id}=resizeBy.body;
+        const orderInfo=await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if (orderInfo.status==='paid') {
+            const transactionData=await transactionModel.findById(orderInfo.receipt)
+
+            if(transactionData.payment){
+                return res.json({success:false,message:'payment false'})
+            }
+            const userData=await userModel.findById(transactionData.userId)
+
+            const creditBalance =userData.creditBalance+transactionData.credits
+
+            await userModel.findByIdAndUpdate(userData._id,{creditBalance})
+            await transactionModel.findByIdAndUpdate
+            (transactionData._id,{payment:true})
+
+            res.json({success:true,message:"credits added"})
+
+
+        }else{
+            res.json({success:false,message:"payment failed"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
+
+export { registerUser, loginUser, userCredits ,paymentRazorpay,verifyRazorpay}
